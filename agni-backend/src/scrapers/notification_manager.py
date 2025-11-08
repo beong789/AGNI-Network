7b96083f4bd932a1d9340ce084c4dbb66e061905
Jose Velazquez <josevelazquez@eduroam-bowers-128-114-154-137.ucsc.edu>
2025-11-08 10:16:50 -0800

    finsihed notifictation system

--- agni-backend/src/scrapers/notification_manager.py
+++ agni-backend/src/scrapers/notification_manager.py
@@ -1,41 +1,30 @@
 from pandas import *
 import smtplib
+import os
 
 MY_EMAIL = "agnifirealert@gmail.com"
 MY_PASSWORD = "qggyedfqthksyyoq"
 TO_ADDRESS = "jgvelazq@ucsc.edu"
 
-data = read_csv("california_fire_danger_factors.csv")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+csv_path = os.path.join(script_dir, "california_fire_danger_factors.csv")
+
+data = read_csv(csv_path)
 data_dict = data.to_dict("records")
-danger_dict = {item.get("county"): item.get("fire_danger_level") for item in data_dict}
+danger_dict = {item.get("county"): item.get("risk_score") for item in data_dict}
 # print(danger_dict)
 
 for county in danger_dict:
     # print(danger_dict.get(county))
     # print(county)
-    if danger_dict.get(county) == "High":
+    message = f"""
+This is an automated message from the Fire Danger Monitoring System \"Agni\".\n
+Our latest data analysis indicates that {county} County is currently experiencing high fire danger conditions based on current weather and environmental factors.\n
+These conditions suggest increased potential for wildire ignition and spread. We recommend reviewing current readiness levels, public advisories, and field conditions within your jurisdiction\n
+Please take any necessary preventative actions and notify response teams if needed.\nThank you for your continued work in protecting the community.\n
+Stay safe,\nAgni\n(agnifirealert@gmail.com)"""
+    if danger_dict.get(county) >= 7:
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(MY_EMAIL, MY_PASSWORD)
-            connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_ADDRESS, msg=f"Subject:High Fire Danger\n\n{county} currently has high fire danger, proactive measures may be required")
-
-# SMTP_HOST = "smtp.gmail.com"
-# SMTP_PORT = 587  # STARTTLS
-# TIMEOUT = 30
-
-# SENDER = MY_EMAIL
-# APP_PASSWORD = MY_PASSWORD
-# TO = TO_ADDRESS
-
-# msg = EmailMessage()
-# msg["From"] = SENDER
-# msg["To"] = TO
-# msg["Subject"] = "Test"
-# msg.set_content("Hello via Gmail SMTP (587).")
-
-# with smtplib.SMTP(SMTP_HOST, SMTP_PORT, timeout=TIMEOUT) as smtp:
-#     smtp.ehlo()
-#     smtp.starttls()  # upgrade to TLS
-#     smtp.ehlo()
-#     smtp.login(SENDER, APP_PASSWORD)
-#     smtp.send_message(msg)
+            connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_ADDRESS, msg=f"Subject:High Fire Danger in {county} County\n\n{message}")
